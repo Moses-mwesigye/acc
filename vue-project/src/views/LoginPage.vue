4bf2bdf75cbcb607fbbc61c66277bba37861cfc2
moses-mwesigye <mwesigyemoses256@gmail.com>
2026-02-06 00:06:26 +0300

    logo

--- vue-project/src/views/LoginPage.vue
+++ vue-project/src/views/LoginPage.vue
@@ -9,5 +9,22 @@ import LoginForm from '@/components/LoginForm.vue'
       <span class="login-status">Not logged in</span>
     </header>
     <LoginForm />
+    <div class="login-logo">
+      <img src="/bww-logo.png" alt="BWWS Logo" />
+    </div>
   </div>
 </template>
+
+<style scoped>
+.login-logo {
+  display: flex;
+  justify-content: center;
+  margin-top: 2rem;
+  padding: 1rem;
+}
+.login-logo img {
+  max-width: 280px;
+  height: auto;
+  display: block;
+}
+</style>
